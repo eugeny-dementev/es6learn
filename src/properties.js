e7d8108e608205c2308d08e844bf38c056ee85e4
Дементьев Евгений Владимирович <eugeny.dementev@ya.ru>
2015-05-23 22:49:13 +0600

    shorthand are good, not evil, good

--- src/properties.js
+++ src/properties.js
@@ -11,3 +11,19 @@ var obj2 = {
 };
 
 console.log(prop[obj2]); // wow! this print 'helo' "D
+
+// and little about shorthand
+
+var lamda = () => console.log('hello from lamda');
+
+var shorthand = {
+  obj,
+  obj2,
+  prop,
+  lamda,
+  helo() {
+    console.log(this.prop[this.obj], this.prop[this.obj] == this.prop[this.obj2]);
+  }
+}
+
+shorthand.helo();
