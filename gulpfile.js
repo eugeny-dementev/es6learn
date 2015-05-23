f018b8cc60cd857166496b0b420a55fab6b0d7dc
Дементьев Евгений Владимирович <eugeny.dementev@ya.ru>
2015-05-23 23:26:31 +0600

    add runtime for generators, async, Promise, Map and Set

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ var gulp = require('gulp'),
 
 gulp.task('default', function() {
 	return gulp.src('src/**')
-		.pipe(babel({modules: 'common'}))
+		.pipe(babel({modules: 'common', optional: ['runtime']}))
 		.pipe(gulp.dest('out'));
 });
