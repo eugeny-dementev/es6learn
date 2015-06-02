9a33fb8565224f44d69e239593369e251d9b1fec
Дементьев Евгений Владимирович <eugeny.dementev@ya.ru>
2015-06-02 10:27:27 +0600

    sideback style

--- src/bind.js
+++ src/bind.js
@@ -2,6 +2,11 @@ var arr = [1, 2, 3, 4, 6, 7];
 
 arg(...arr); // .apply
 
+arr::function() {
+  // callbacks are douchbags, sidebacks our future :D :crazy:
+  arg(...this::[].slice(2));
+}();
+
 function arg() {
   console.log(arguments::[].slice(2)); // .call
 }
