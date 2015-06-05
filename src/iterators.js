d7f34b8517caf43c44f69cf009d987e529550049
Дементьев Евгений Владимирович <eugeny.dementev@ya.ru>
2015-06-05 21:29:55 +0600

    iterators with spread

--- src/iterators.js
+++ src/iterators.js
@@ -1,14 +1,31 @@
-var iterator = {
+var iterable = { // iterable object is any object with [Symbol.iterator] method.
+  // this method must return
   [Symbol.iterator]() {
-    let i = 0;
+    // We can create it by just write method [Symbol.iterator] in any object,
+    // and result of this method must be a object with methods next and optional return, throw.
+    // In babel throw dont work (need to check)
+    let i = 0,
+      done = false;
     return {
       next() {
-        return { done: i < 5 ? false : true, value: i++ };
+        if (i > 10) done = true;
+        return { done, value: i++ };
+      },
+      return() {
+        done = true;
+        return { done };
       }
     }
   },
   hello: 'hello'
 };
+
+
+
+var iterator = iterable[Symbol.iterator]();
+// [Symbol.iterator] must return object with method next()
+// and optional methods return() and throw().
+// this object named iterator
 
 for (let prop in iterator)
   console.log(prop); // Symbol is enumerable property
@@ -16,6 +33,9 @@ for (let prop in iterator)
 for (let prop in iterator) // and its own property
   if (iterator.hasOwnProperty(prop)) console.log(prop);
 
-console.log(iterator);
+for (let i of iterable) {
+  console.log('value: ', i);
+  if (i > 4) break;
+}
 
-for (let i of iterator) console.log('value: ', i);
+console.log([...iterable]);
