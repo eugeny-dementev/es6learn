2d2ab7c2384f778bfd1417dc143c97e3741a7449
Дементьев Евгений Владимирович <eugeny.dementev@ya.ru>
2015-06-07 20:54:53 +0600

    weak map is cool

--- src/collections.js
+++ src/collections.js
@@ -17,3 +17,25 @@ console.log('entries obj:', entries); // iterator, looks like empty obj
 console.log('iterator obj:', iterator); // another iterator
 console.log('compare of it and ent:', iterator == entries);
 console.log('entries.next func:', entries.next); // but method exist of course
+
+var val = {helo: 'hello'};
+
+/*
+ * WeakMap uses link on object only for keys.
+ * this need for observe value memory place and
+ * make possible to delete it if by GC if no other links in code
+ */
+
+var weakMap = new WeakMap();
+
+weakMap.set(val, val);
+
+console.log('weakMap size before func:', weakMap.length);
+checkWeakness();
+console.log('weakMap size after func:', weakMap.length);
+
+function checkWeakness() {
+  var value = {obj: {hello: 'hello'}};
+  weakMap.set(value, value);
+  console.log('weakMap size in func:', weakMap.length);
+}
