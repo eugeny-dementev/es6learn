eac49caa282a63e27ce9cddb0c572003f6440888
Дементьев Евгений Владимирович <eugeny.dementev@ya.ru>
2015-06-10 20:46:10 +0600

    transformers power

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
-var gulp = require('gulp'),
-    babel = require('gulp-babel');
+var _ = require('lodash'),
+  gulp = require('gulp'),
+  babel = require('gulp-babel')
+
+var babelConfig = {modules: 'common', optional: ['runtime'], stage: 0}
 
 gulp.task('default', function() {
 	return gulp.src('src/**')
-		.pipe(babel({modules: 'common', optional: ['runtime'], stage: 0}))
+		.pipe(babel(babelConfig))
+		.pipe(gulp.dest('out'));
+});
+
+gulp.task('forof', function() {
+  var forofAllow = {
+    blacklist: ['es6.forOf']
+  }
+
+  return gulp.src('src/**')
+    .pipe(babel(_.extend(babelConfig, forofAllow)))
 		.pipe(gulp.dest('out'));
 });
---
+++ src/forof.js
@@ -0,0 +1,4 @@
+var arr = [1, 2, 3, 4, 5];
+
+for (let i of arr)
+  console.log(`value of i: ${i}`);
