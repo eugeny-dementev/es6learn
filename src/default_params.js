b98c6df475dedd3facfac2a39728575b412667c4
Eugeny Dementev <eugeny.dementev@ya.ru>
2015-06-14 17:18:43 +0600

    default destruct object param

--- src/default_params.js
+++ src/default_params.js
@@ -1,6 +1,12 @@
 print(true);
 print();
 
+defObj();
+
 function print(print = false) {
   if (print) console.log('print is true');
+}
+
+function defObj({date, count} = {date: new Date(), count: 0}) {
+  console.log(`date is ${date} and count is ${count}`);
 }
