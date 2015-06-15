1ae3d98743ddca55dfebbfee2f0409981b611f40
Eugeny Dementev <eugeny.dementev@ya.ru>
2015-06-15 17:18:43 +0600

    default destruct array param

--- src/default_params.js
+++ src/default_params.js
@@ -2,11 +2,16 @@ print(true);
 print();
 
 defObj();
+defArr();
 
 function print(print = false) {
   if (print) console.log('print is true');
 }
 
 function defObj({date, count} = {date: new Date(), count: 0}) {
+  console.log(`date is ${date} and count is ${count}`);
+}
+
+function defArr([date, count] = [new Date(), 0]) {
   console.log(`date is ${date} and count is ${count}`);
 }
