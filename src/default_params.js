0e0766bb86ad620d42440cebce4f93aa3bb072d2
Eugeny Dementev <eugeny.dementev@ya.ru>
2015-06-20 18:35:55 +0600

    default rest array param lol

--- src/default_params.js
+++ src/default_params.js
@@ -3,6 +3,7 @@ print();
 
 defObj();
 defArr();
+defSpreadArr();
 
 function print(print = false) {
   if (print) console.log('print is true');
@@ -14,4 +15,8 @@ function defObj({date, count} = {date: new Date(), count: 0}) {
 
 function defArr([date, count] = [new Date(), 0]) {
   console.log(`date is ${date} and count is ${count}`);
+}
+
+function defSpreadArr([...params] = [1, 2, 3]) {
+  console.log(`params is ${params}`);
 }
