4a374fd93cf04d9962709e520f20a741815d31e8
Eugeny Dementev <eugeny.dementev@ya.ru>
2015-06-20 15:51:18 +0600

    function too

--- src/block_scoped.js
+++ src/block_scoped.js
@@ -1,16 +1,27 @@
 let bsVar = 'world';
 
+console.log(foo());
+
 {
   let bsVar = 'hello';
 
-  console.log(bsVar);
-  console.log('\b');
+  console.log(foo());
 
   {
     const bsVal = 'cosnting';
     // bsVal = 'unconst'; // error when compiling
     // if make lib, of course have a "problem
   }
+
+  console.log(bsVar);
+
+  function foo() {
+    return 'from jail';
+  }
+}
+
+function foo() {
+  return 'from out';
 }
 
 console.log(bsVar);
