2b50da3e679cdce3006adab6bea579c50ee16e81
Eugeny Dementev <eugeny.dementev@ya.ru>
2015-06-20 15:50:45 +0600

    import 'name' not important, just like in CommonJS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import miniEmitter from './lib/emitter.js';
-import Logger from './lib/logger.js';
+import Log from './lib/logger.js';
+import _ from 'lodash';
 
 var handler = new miniEmitter,
-    logger = new Logger(console.log);
+    logger = new Log(console.log);
 
 handler.on('h', () => logger.log('miniEmitter is alive!!!!11'));
 
 handler.emit('h');
+
+_.map([1, 2, 3], elem => console.log(elem));
 
