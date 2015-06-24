6af9ba60bba97c0b54caa48115cdabd3629a2b47
Eugeny Dementev <eugeny.dementev@ya.ru>
2015-06-24 23:34:09 +0600

    fix typo

--- src/nosctirct.js
+++ src/nosctirct.js
@@ -1,10 +1,10 @@
-// if i use non scritc "feature"
+// if i use non-strict "feature"
 
 showThis();
 
 function showThis() {
   // no strict file, but
-  // after babel this == undefined
+  // after babel this === undefined
   console.log('this', this);
   // Possible problems if your code is gavno
 }
