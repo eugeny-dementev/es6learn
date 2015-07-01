0052fdc94f51b55aca1657af20f2d0f6a000f5a8
Eugeny Dementev <eugeny.dementev@ya.ru>
2015-07-01 19:35:31 +0600

    possible arrow funk

---
+++ src/presentation/possible_arrows.js
@@ -0,0 +1,8 @@
+
+let a = (a, b) => { let c = a + b; return c; };
+let b = (a, b) => a + b;
+let c = a => a + 1;
+let d = a => { let b = a + 1; return b; };
+let e = obj => ({ obj: obj });
+
+let f = f => ({f});
