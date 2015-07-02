5c14279030a3ec1062827595c9f3f742d8c3c273
Eugeny Dementev <eugeny.dementev@ya.ru>
2015-07-02 20:55:13 +0600

    improve example

--- src/presentation/classes_and_functions.js
+++ src/presentation/classes_and_functions.js
@@ -3,13 +3,23 @@ function Point(x, y) {
   this.y = y;
 }
 
+Point.prototype.toString = function toString() {
+  return 'x: ' + this.x + ', y: ' + this.y;
+};
+
 class superPoint extends Point {
   constructor(x, y, z) {
     super(x, y);
     this.z = z;
   }
+
+  toString() {
+    return `${super.toString()}, z: ${this.z}`;
+  }
 }
 
 let point = new superPoint(10, 10, 10);
 
-console.log(point);
+console.log('' + point); // x: 10, y: 10, z: 10
+
+
