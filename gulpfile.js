c8a6a63cd647f813cdef4ebb7d6e77085fab3658
Eugeny Dementev <eugeny.dementev@ya.ru>
2015-07-02 20:54:36 +0600

    butify

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
 var _ = require('lodash'),
   gulp = require('gulp'),
   uglify = require('gulp-uglify'),
-  babel = require('gulp-babel')
+  babel = require('gulp-babel');
 
-var babelConfig = {modules: 'common', optional: ['runtime'], stage: 0}
+var babelConfig = {modules: 'common', optional: ['runtime'], stage: 0};
 
 gulp.task('default', function() {
-	return gulp.src('src/**')
-		.pipe(babel(babelConfig))
-		.pipe(gulp.dest('out'));
+  return gulp.src('src/**')
+    .pipe(babel(babelConfig))
+    .pipe(gulp.dest('out'));
 });
 
 gulp.task('forof', function() {
   var forofAllow = {
     blacklist: ['es6.forOf']
-  }
+  };
 
   return gulp.src('src/**')
     .pipe(babel(_.extend(babelConfig, forofAllow)))
-		.pipe(gulp.dest('out'));
+    .pipe(gulp.dest('out'));
 });
 
 gulp.task('ugly6', function() {
